8f62cfe6d620395043fe101dde3b33051c85603c
Stanislav <stanislavbabalev96@gmail.com>
2018-01-24 19:39:27 +0200

    Hours and reservehour added

---
+++ Dentist/Dentist/Areas/Dentist/Models/ReserveHourModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dentist.Areas.Dentist.Models
+{
+    public class ReserveHourModel
+    {
+        public string DentistName { get; set; }
+        public string UserID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        [Required(ErrorMessage = "The date is required")]
+        public DateTime Hour { get; set; }
+
+       
+    }
+}
---
+++ Dentist/Dentist/Areas/Users/Models/Hours.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dentist.Areas.Users.Models
+{
+    public class Hours
+    {
+        public string DentistName { get; set; }
+        public string UserID { get; set; }
+      
+        public DateTime Hour { get; set; }
+    }
+}
